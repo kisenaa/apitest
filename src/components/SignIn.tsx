38455ec50ee68d3cab5541d24cc61be4ef7c050b
Johannes Daniswara Pratama <115395772+kisenaa@users.noreply.github.com>
2023-10-26 23:25:10 +0700

    add bg

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,5 +1,6 @@
 import { SignIn } from '@clerk/clerk-react';
 import { ReactNode } from 'react';
+import bg from '../assets/images/bg.jpg'
 
 const SignIns = ({ children }: { children?: ReactNode }) => {
   return (
@@ -7,11 +8,11 @@ const SignIns = ({ children }: { children?: ReactNode }) => {
       <div
         className="flex h-screen bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: "url('../src/assets/images/bg.jpg')",
+          backgroundImage: `url(${bg})`,
         }}
       ></div>
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <SignIn routing="path" path="/sign-in" afterSignInUrl="/dashboard"/>
+        <SignIn routing="path" path="/sign-in" afterSignInUrl="/dashboard" redirectUrl="/dashboard"/>
         {children}
       </div>
     </div>
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,5 +1,6 @@
 import { SignUp } from '@clerk/clerk-react';
 import { ReactNode } from 'react';
+import bg from '../assets/images/bg.jpg'
 
 const SignUps = ({ children }: { children?: ReactNode }) => {
   return (
@@ -7,11 +8,11 @@ const SignUps = ({ children }: { children?: ReactNode }) => {
       <div
         className="flex h-screen bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: "url('../src/assets/images/bg.jpg')",
+          backgroundImage: `url(${bg})`,
         }}
       ></div>
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <SignUp routing="path" path="/sign-up" afterSignUpUrl="/dashboard"/>
+        <SignUp routing="path" path="/sign-up" afterSignUpUrl="/dashboard" redirectUrl="/dashboard"/>
         {children}
       </div>
     </div>
