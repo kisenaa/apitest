8bc6741bbe875097c03710924de5aae326739e43
Johannes Daniswara Pratama <115395772+kisenaa@users.noreply.github.com>
2023-10-26 22:07:09 +0700

    update

--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -1,10 +1,4 @@
-import {
-  ClerkProvider,
-  RedirectToSignIn,
-  SignedIn,
-  SignedOut,
-} from '@clerk/clerk-react';
-import dotenv from 'dotenv';
+import { ClerkProvider, RedirectToSignIn,SignedIn, SignedOut } from '@clerk/clerk-react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 
 import SignIns from '@/components/SignIn';
@@ -13,11 +7,12 @@ import SignUps from '@/components/SignUp';
 import Dashboard from '@/pages/dashboard/Dashboard';
 import HomePage from '@/pages/HomePage';
 
-dotenv.config()
-const clerkPubKey = `${process.env.REACT_APP_CLERK_PUBLISHABLE_KEY}`;
+const clerkPubKey = `${await import.meta.env
+  .VITE_REACT_APP_CLERK_PUBLISHABLE_KEY}`;
 
-const AllRoutes = () => {
+  const AllRoutes = () => {
   const navigate = useNavigate();
+
 
   return (
     <ClerkProvider publishableKey={clerkPubKey} navigate={(to) => navigate(to)}>
@@ -25,19 +20,17 @@ const AllRoutes = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/sign-in/*" element={<SignIns />} />
         <Route path="/sign-up/*" element={<SignUps />} />
-        <Route
-          path="/dashboard/*"
-          element={
-            <>
-              <SignedIn>
-                <Dashboard />
-              </SignedIn>
-              <SignedOut>
-                <RedirectToSignIn redirectUrl="/dashboard/" />
-              </SignedOut>
-            </>
-          }
-        />
+        <Route path="/home/*" element={<HomePage />} />
+        <Route path='/dashboard/*' element={
+          <>
+            <SignedIn>
+              <Dashboard/>
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn redirectUrl='/dashboard/'/>
+            </SignedOut>
+          </>
+        } />
       </Routes>
     </ClerkProvider>
   );
